b88c36d30b1e3bce272f13763cc6da4f3ea06199
Madhu HS <86066580+Madhu-2101@users.noreply.github.com>
2024-06-04 18:35:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings: ['A Student.', 'A Learner.', 'And Unemployed.'],
+    strings: ['A Student.', 'And Learner.'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
